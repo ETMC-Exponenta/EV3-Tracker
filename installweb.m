03f54f7d8a92e86c3f5959315e44174a929dd7b8
Pavel Roslovets <roslovets@exponenta.ru>
2020-03-02 10:21:15 +0300

    Create installweb.m

---
+++ installweb.m
@@ -0,0 +1,15 @@
+instvar.URL = 'https://github.com/ETMC-Exponenta/EV3-Tracker/archive/master.zip';
+instvar.Name = 'EV3-Tracker';
+% Main routine
+fprintf('Downloading %s...\n', instvar.Name);
+websave(instvar.Name, instvar.URL);
+disp('Installing...')
+disp('Download complete, installing...')
+instvar.Fs = unzip(instvar.Name);
+instvar.DName = instvar.Fs{1}(1:end-1);
+movefile(instvar.DName, instvar.Name);
+cd(instvar.Name)
+disp('Install complete, have fun!')
+clear instvar
+% Post-install tasks
+open('EV3-Tracker.prj')
